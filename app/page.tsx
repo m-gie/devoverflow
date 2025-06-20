c33c6eca5e0f6adbace6cf5165f1b13f86ecb3c4
Rohit-Palavanchu <rohitp.vja@gmail.com>
2025-06-20 09:15:11 +0530

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-[var(--color-primary-500)] font-space-grotesk">Hi Next</h1>
+      <h1 className=" font-inter px-2 py-2 border-amber-300 border-8">Hi Next</h1>
     </>
   );
 }
